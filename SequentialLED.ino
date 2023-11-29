274befc9d43f29c0a9b975f4a0aee577af91af65
Alec Jones <akj123.kjp@gmail.com>
2023-11-29 00:48:41 -0400

    Ajuster l'ordre dans laquelle les DEL s'allumes

--- SequentialLED.ino
+++ SequentialLED.ino
@@ -1,6 +1,6 @@
 int input = A0; //Le signal analogique est lu sur la pin A0
-int nLED = 4; //Nombre de LED
-int premierPin = 2; //Première pin utilisée pour les LED
+int nLED = 4; //Nombre de DEL
+int premierPin = 2; //Première pin utilisée pour les DEL
 
 
 void setup() {
@@ -16,14 +16,13 @@ void loop() {
     //On lit la valeur du signal analogique
     int val = analogRead(input);
 
-    //On allume les LED en fonction de la valeur lue
+    //On allume les DEL en fonction de la valeur lue
     for (int i = 0; i < nLED; i++)
     {
-        if (val <= i*(1024/nLED))
+        if (val >= (i+1)*(1023/nLED))
         {
-            digitalWrite((i + premierPin), LOW);
+            digitalWrite((i + premierPin), HIGH);
         }
-        else digitalWrite((i + premierPin), HIGH);
+        else digitalWrite((i + premierPin), LOW);
     }
-    Serial.println(val);
 }
